6e672c4ed685b79db98438e1ac58fe548fa87594
Maik Becker <maik.becker@tu-dortmund.de>
2016-12-17 00:31:48 +0100

    Auswertung erweitert

--- V57 Elektrische Schwankungserscheinungen/analysis/auswertung_teil_2.py
+++ V57 Elektrische Schwankungserscheinungen/analysis/auswertung_teil_2.py
@@ -10,12 +10,13 @@ plt.rc('text', usetex=True)
 plt.rc('font', family='serif', size=16)
 
 ################################################################################
-### Oxydkathode ################################################################
+### Oxydkathode
 ################################################################################
 
 ν, VS, VN, U = np.genfromtxt('oxydkathode.txt', unpack = True)
 Δν = frequency2bandwidth(ν, 'Oxydkathode')
 U = normVoltage(U, VS, VN)
+    # Vielleicht zwecks Übersichtlichkeit besser auf VZ = 100 normieren. Mal schauen...
 R = 2200
 W = U / (R ** 2 * Δν)
 
@@ -32,16 +33,61 @@ plt.savefig('oxydkathode_frequenzspektrum.pdf')
 plt.cla()
 plt.clf()
 
-params, cov = curve_fit(linearFunction, np.log(ν), np.log(noms(W)))
+params, cov = curve_fit(linearFunction, np.log(ν[-16:]), np.log(noms(W[-16:])))
+    # Wähle hier und im Folgenden mit [-16:] alle Werte mit f < 1000 Hz aus
 errors = np.sqrt(np.diag(cov))
 m = ufloat(params[0], errors[0])
 b = ufloat(params[1], errors[1])
+print('Der Exponent alpha wird zu {} bestimmt'.format(m))
 
-print(m)
+x = np.linspace(1, 2000, 2)
+plt.plot(ν[-16:], noms(W[-16:]), 'rx', label = 'Messdaten')
+plt.plot(x, x ** noms(m) * np.exp(noms(b)), 'b-', label = 'Ausgleichsgerade')
+    # Genau so, weil ln(y)=m*ln(x)+b <=> y=x**m*exp(b)
+plt.xlabel(r'$\nu\,/\,\mathrm{Hz}$')
+plt.ylabel(r'$W(\nu)\,/\,\mathrm{A^2s}$')
+plt.xscale('log')
+plt.yscale('log')
+plt.xlim(1, 2000)
+plt.grid()
+plt.legend(loc = 'best')
+plt.tight_layout()
+plt.title('Oxydkathode - Frequenzspektrum (linearer Teil)')
+plt.savefig('oxydkathode_frequenzspektrum_linearer_teil.pdf')
+plt.cla()
+plt.clf()
 
-x = np.linspace(np.log(min(ν)), np.log(max(ν)), 2)
+################################################################################
+### Reinmetallkathode
+################################################################################
+
+U   = [10, 18, 25, 35, 43, 60, 75, 92, 110, 125]
+I60 = [0.4, 0.45, 0.45, 0.49, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]
+I62 = [0.6, 0.7, 0.7, 0.7, 0.7, 0.72, 0.72, 0.75, 0.75, 0.76]
+I64 = [0.8, 0.9, 0.9, 0.92, 0.92, 0.97, 0.98, 1.0, 1.0, 1.0]
+I65 = [0.9, 1.2, 1.21, 1.24, 1.25, 1.28, 1.3, 1.31, 1.31, 1.33]
+
+plt.plot(U, I60, 'rx', label = r'$I_{\mathrm{H}}=0.60\mathrm{A}$')
+plt.plot(U, I62, 'bx', label = r'$I_{\mathrm{H}}=0.62\mathrm{A}$')
+plt.plot(U, I64, 'gx', label = r'$I_{\mathrm{H}}=0.64\mathrm{A}$')
+plt.plot(U, I65, 'kx', label = r'$I_{\mathrm{H}}=0.65\mathrm{A}$')
+plt.xlabel(r'$U\,/\,\mathrm{V}$')
+plt.ylabel(r'$I\,/\,\mathrm{mA}$')
+plt.ylim(0.4, 1.6)
+plt.grid()
+plt.legend(loc = 'best', ncol = 2, fontsize = 16)
+plt.title('Reinmetallkathode - Charakteristische Linien')
+plt.savefig('reinmetallkathode_linien.pdf')
+plt.cla()
+plt.clf()
+
+ν, VS, VN, U = np.genfromtxt('reinmetallkathode.txt', unpack = True)
+Δν = frequency2bandwidth(ν, 'Reinmetallkathode')
+U = normVoltage(U, VS, VN)
+R = 4680
+W = U / (R ** 2 * Δν)
+
 plt.plot(ν, noms(W), 'rx', label = 'Messdaten')
-plt.plot(x, linearFunction(x, *params), 'b-')
 plt.xlabel(r'$\nu\,/\,\mathrm{Hz}$')
 plt.ylabel(r'$W(\nu)\,/\,\mathrm{A^2s}$')
 plt.xscale('log')
@@ -49,8 +95,8 @@ plt.yscale('log')
 plt.grid()
 plt.legend(loc = 'best')
 plt.tight_layout()
-plt.title('Oxydkathode - Frequenzspektrum (linearer Teil)')
-plt.savefig('oxydkathode_frequenzspektrum_linearer_teil.pdf')
+plt.title('Reinmetallkathode - Frequenzspektrum')
+plt.savefig('reinmetallkathode_frequenzspektrum.pdf')
 plt.cla()
 plt.clf()
 
@@ -76,55 +122,6 @@ sys.exit()
 
 
 
-
-
-
-
-
-
-
-# Reinmetallkathode - Charakteristische Linien
-
-U = [10, 18, 25, 35, 43, 60, 75, 92, 110, 125]
-
-I60 = [0.4, 0.45, 0.45, 0.49, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]
-I62 = [0.6, 0.7, 0.7, 0.7, 0.7, 0.72, 0.72, 0.75, 0.75, 0.76]
-I64 = [0.8, 0.9, 0.9, 0.92, 0.92, 0.97, 0.98, 1.0, 1.0, 1.0]
-I65 = [0.9, 1.2, 1.21, 1.24, 1.25, 1.28, 1.3, 1.31, 1.31, 1.33]
-
-plt.plot(U, I60, 'rx', label=r'$I_{\mathrm{H}}=0.60\mathrm{A}$')
-plt.plot(U, I62, 'bx', label=r'$I_{\mathrm{H}}=0.62\mathrm{A}$')
-plt.plot(U, I64, 'gx', label=r'$I_{\mathrm{H}}=0.64\mathrm{A}$')
-plt.plot(U, I65, 'kx', label=r'$I_{\mathrm{H}}=0.65\mathrm{A}$')
-plt.xlabel(r'$U\,/\,\mathrm{V}$')
-plt.ylabel(r'$I\,/\,\mathrm{mA}$')
-plt.ylim(0.4, 1.8)
-plt.grid()
-plt.legend(loc='best')
-plt.title('Reinmetallkathode - Charakteristische Linien')
-plt.savefig('reinmetallkathode_linien.pdf')
-plt.cla()
-plt.clf()
-#plt.show()
-
-# Reinmetallkathode - Frequenzspektrum
-
-ν, VS, VN, U = np.genfromtxt('reinmetallkathode.txt', unpack=True)
-Δν = [frequency2bandwidth(x, 'Reinmetallkathode') for x in ν]
-U = normVoltage(U, VS, VN)
-R = 4680
-W = [x / (R ** 2 * y) for x, y in zip(U, Δν)]
-
-plt.plot(ν, noms(W), 'rx')
-plt.xlabel(r'$\nu\,/\,\mathrm{Hz}$')
-plt.ylabel(r'$W(\nu)\,/\,\mathrm{A^2s}$')
-plt.xscale('log')
-plt.yscale('log')
-plt.title('Reinmetallkathode - Frequenzspektrum')
-plt.savefig('reinmetallkathode_frequenzspektrum.pdf')
-plt.cla()
-plt.clf()
-#plt.show()
 
 # Bestimmung der Elementarladung
 
